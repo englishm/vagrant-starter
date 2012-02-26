047d92350e7adb53c9967ba486a58ef0614ee06c
Mike English <mike.english@atomicobject.com>
2012-02-26 15:00:01 -0500

    make a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullStackDemo::Application.config.secret_token = '0e7e2360d470c112496e4fb7f9b8c814d87b3d0c182404b0e30ccff3157cdff44d8052a3d1ff71934df2a405f2df05f6c384ae9f6b542d77c4c28439c274bd21'
