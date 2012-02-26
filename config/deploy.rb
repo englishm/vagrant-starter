da7f61f9b1cbf85244132c0962dff0b58faa3b5b
Mike English <mike.english@atomicobject.com>
2012-02-26 18:52:39 -0500

    fill in a deploy.rb for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
+set :application, "demo-app"
+set :repository, "http://github.com/gazoombo/demo-app.git" 
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+role :web
+role :app
+role :db
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+task :production do
+  server "www.example.com", :app, :web, :db
+  set :deploy_to, "/home/user/sites/demo-app"
+  set :deploy_via, :remote_cache
+end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+task :vagrant do
+  server "33.33.33.10", :app, :web, :db
+  set :user, 'vagrant'
+  ssh_options[:keys] = `vagrant ssh-config | grep IdentityFile`.split.last
+  set :deploy_to, "/home/vagrant/sites/demo-app"
+  set :deploy_via, :remote_cache
+end
